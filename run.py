90e4aeb567610ccc0ebd225fd28fdf0b13fabeb4
Wiktor <65229344+WKR92@users.noreply.github.com>
2021-01-14 17:34:07 +0100

    new

--- run.py
+++ run.py
@@ -257,11 +257,10 @@ def loadingPage():
 @app.route('/reader', methods=['GET', 'POST', 'PUT'])
 def reader():
     global dataSession
-    data = dataSession
     bookTittle = session.get('bookTittle', None)
     flash('Your file is uploaded. Have a nice read.', "success")
 
-    return render_template('reader.html', title='Web Reader', data=json.dumps(data), bookTitle = json.dumps(bookTittle))
+    return render_template('reader.html', title='Web Reader', data=json.dumps(dataSession), bookTitle = json.dumps(bookTittle))
 
 
 #command to check how many free dyno hours i have left:
