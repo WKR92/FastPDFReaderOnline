2855857113966fbf95e00304d84f9939d549cb66
Wiktor <65229344+WKR92@users.noreply.github.com>
2021-01-14 15:11:52 +0100

    new

--- run.py
+++ run.py
@@ -42,8 +42,8 @@ def save_pdf(pdf_form):
     f_name, f_ext = os.path.splitext(pdf_form.filename)
     pdf_fn = f_name + f_ext
     # underneath line is for local version
-    pdf_path = os.path.join(app.root_path, 'static/user_pdf', pdf_fn)
-    # pdf_path = os.path.join(app.root_path, 'tmp')
+    # pdf_path = os.path.join(app.root_path, 'static/user_pdf', pdf_fn)
+    pdf_path = os.path.join(app.root_path, 'tmp')
     pdf_form.save(pdf_path)
 
     return pdf_path
